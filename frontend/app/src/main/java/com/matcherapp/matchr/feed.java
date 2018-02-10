b51668d34e35f5cd0d3a6ab3a2a821ffd213692d
chrisdean258 <chrisdean258@gmail.com>
2018-02-10 14:10:37 -0500

    frontend project created

---
+++ frontend/app/src/main/java/com/matcherapp/matchr/feed.java
@@ -0,0 +1,13 @@
+package com.matcherapp.matchr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
