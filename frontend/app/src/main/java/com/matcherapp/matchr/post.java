43c42df3ad71275dc4230320d384ed605f0b1c1f
chrisdean258 <chrisdean258@gmail.com>
2018-02-11 09:01:06 -0500

    Final Product

--- frontend/app/src/main/java/com/matcherapp/matchr/post.java
+++ frontend/app/src/main/java/com/matcherapp/matchr/post.java
@@ -7,40 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class post extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    ImageView uploadImageView;
+        @Override
+        protected void onCreate(final Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_post);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post);
-
-        final Button submit = (Button)findViewById(R.id.submitBtn);
-        submit.setOnClickListener(new View.OnClickListener() {
+            final Button submit = (Button) findViewById(R.id.submitBtn);
+            submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent submitIntent;
+                    Intent submitIntent;
+                    submitIntent = new Intent(getApplicationContext(), Navigation.class);
+                    startActivity(submitIntent);
+                }
+            });
 
-                submitIntent = new Intent(getApplicationContext(), Navigation.class);
-
-                startActivity(submitIntent);
-            }
-        });
-
-        ImageButton add = (ImageButton)findViewById(R.id.addImageButton);
+        ImageButton add = (ImageButton) findViewById(R.id.addImageButton);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, 1);
                 }
             }
         });
     }
-}
+
+    }
