cf4747daf379e88d2751da5025372c3f519417e1
chrisdean258 <chrisdean258@gmail.com>
2018-02-10 23:41:22 -0500

    class files

---
+++ frontend/app/src/main/java/com/matcherapp/matchr/Post1.java
@@ -0,0 +1,36 @@
+package com.matcherapp.matchr;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Post1 {
+
+    String shirtURL;
+    int postID;
+    String user;
+    Pants[] pants;
+
+
+    public Post1(JSONObject obj)
+    {
+        try {
+            shirtURL = obj.getString("shirtURL");
+            postID = obj.getInt("postID");
+            user = obj.getString("user");
+            pants = getPants(obj);
+        } catch(JSONException e) {}
+
+    }
+
+    private Pants[] getPants(JSONObject obj) throws JSONException {
+        JSONArray pants = obj.getJSONArray("pants");
+        Pants[] rtn = new Pants[pants.length()];
+        for(int i = 0 ; i < pants.length(); i++)
+        {
+            rtn[i] = new Pants(pants.getJSONObject(i));
+        }
+        return rtn;
+    }
+}
