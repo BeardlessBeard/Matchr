75c2de025027c0aea2d5e43f09674afe0c083ceb
kveca1 <kveca1@vols.utk.edu>
2018-02-11 12:27:32 -0500

    added submit button on voting pages

--- frontend/app/src/main/java/com/matcherapp/matchr/shirt2.java
+++ frontend/app/src/main/java/com/matcherapp/matchr/shirt2.java
@@ -1,7 +1,10 @@
 package com.matcherapp.matchr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class shirt2 extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class shirt2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shirt2);
+
+        final Button submit = (Button) findViewById(R.id.buttonv);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent submitIntent;
+                submitIntent = new Intent(getApplicationContext(), Navigation.class);
+                startActivity(submitIntent);
+            }
+        });
     }
 }
