a78c531bda86b722bf56f0c013318dbabfe7180c
Will Lifferth <wlifferth@icloud.com>
2018-02-10 21:31:12 -0500

    Added shirt serving, and fixed brent's massive error

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-feed =
-[
+feed = [
     {
         'user': 'William Lifferth',
         'postID': 100001,
@@ -56,8 +55,12 @@ def results_user():
     return json.dumps(feed)
 
 @app.route('/image/<int:post_id>/pants/<filename>')
-def image(post_id, filename):
+def pants_image(post_id, filename):
     return send_file('{}/image/{}/pants/{}'.format(dir_path, post_id, filename))
+
+@app.route('/image/<int:post_id>/shirt.jpg')
+def shirt_image(post_id):
+    return send_file('{}/image/{}/shirt.jpg'.format(dir_path, post_id))
 
 
 if __name__ == '__main__':
