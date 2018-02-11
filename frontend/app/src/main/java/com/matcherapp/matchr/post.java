58af100d0ff819b257cb0eecd52550cd827e778e
chrisdean258 <chrisdean258@gmail.com>
2018-02-11 04:30:38 -0500

    honestly no idea

--- frontend/app/src/main/java/com/matcherapp/matchr/post.java
+++ frontend/app/src/main/java/com/matcherapp/matchr/post.java
@@ -1,10 +1,9 @@
 package com.matcherapp.matchr;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,14 +16,10 @@ public class post extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        uploadImageView = findViewById(R.id.uploadImageView);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        final Button submit;
-
-        submit = (Button)findViewById(R.id.submitButton);
+        final Button submit = (Button)findViewById(R.id.submitBtn);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,18 +39,8 @@ public class post extends AppCompatActivity {
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    onActivityResult(REQUEST_IMAGE_CAPTURE, RESULT_OK, takePictureIntent);
                 }
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            uploadImageView.setImageBitmap(imageBitmap);
-        }
     }
 }
